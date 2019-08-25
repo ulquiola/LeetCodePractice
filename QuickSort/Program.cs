b47113a264c67058d803a434c8abac59308ab18f
ulquiola <ulquiola@163.com>
2019-08-25 12:02:24 +0800

    quickSort

---
+++ QuickSort/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel;
+using System;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static int[] QuickSort(int[] nums,int left,int right)
+        {
+            if(left<right)
+                {
+                    int x =nums[left]; 
+                int i=left;
+                int j=right;
+                while(i<j)
+                {
+                    while(i<j)
+                    {
+                        if(nums[j]<= x)
+                        {
+                            nums[i]=nums[j];
+                            break;
+                        }
+                        else
+                        {
+                            j--;
+                        }
+                    }
+                    while(i<j)
+                    {
+                        if(nums[i]>x)
+                        {
+                            nums[j]=nums[i];
+                            break;
+                        }
+                        else
+                        {
+                            i++;
+                        }
+                    }
+                    nums[i]=x;
+                    QuickSort(nums,left,i-1);
+                    QuickSort(nums,i+1,right);
+                }
+                return nums;   
+            }
+            return null;            
+        }
+        static void Main(string[] args)
+        {
+            int[] nums={1,5,3,2,4};
+            QuickSort(nums,0,nums.Length-1);
+            foreach(var i in nums)
+            {
+                System.Console.WriteLine(i);
+            }
+        }
+    }
+}
