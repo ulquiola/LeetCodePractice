67df27e3637a85255bb30ce962fabdb469933db8
ulquiola <ulquiola@163.com>
2019-08-27 21:56:47 +0800

    lastest.IsVaild

---
+++ IsValid/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Net.NetworkInformation;
+using System.Collections.Generic;
+using System;
+using System.Collections;
+
+namespace IsValid
+{
+    class Program
+    {
+        static bool IsValid(string s)
+        {
+            Stack st = new Stack();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+                {
+                    st.Push(s[i]);
+                }
+                else if (s[i] == ')' || s[i] == ']' || s[i] == '}')
+                {
+                    if (st.Count == 0)
+                    {
+                        return false;
+                    }
+                    char cur = Convert.ToChar(st.Pop());
+                    if (cur == '(' && s[i] != ')')
+                    {
+                        return false;
+                    }
+                    if (cur == '[' && s[i] != ']')
+                    {
+                        return false;
+                    }
+                    if (cur == '{' && s[i] != '}')
+                    {
+                        return false;
+                    }
+                }
+            }
+            if (st.Count == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            string s = "(]";
+            System.Console.WriteLine(IsValid(s));
+        }
+    }
+}
---
+++ longestCommonPrefix/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace longestCommonPrefix
+{
+    class Program
+    {
+        static string longestCommonPrefix(string[] strs)
+        {
+            string lcp = "";
+            if (strs.Length == 0 || strs == null)
+            {
+                return lcp;
+            }
+            string min = strs[0];
+            for(int i=0;i<strs.Length;i++)
+            {
+                if(strs[i].Length<min.Length)
+                {
+                    min=strs[i];
+                }
+            }
+            for (int i = 0; i < min.Length; i++)
+            {
+                for (int k = 0; k < strs.Length; k++)
+                {
+                    if (min[i] != strs[k][i])
+                    {
+                        return lcp;
+                    }
+                }
+                lcp += min[i];
+            }
+            return lcp;
+
+        }
+        static void Main(string[] args)
+        {
+            string[] s = { "ca","a" };
+            System.Console.WriteLine(longestCommonPrefix(s));
+        }
+        
+    }
+}
